6887cd5561389dec0a45d626793fa2a7da37135e
Claudio Scheer <claudioscheer@protonmail.com>
2020-12-13 02:02:31 -0300

    Allow to set web document root

--- src/web_server/web_server.cpp
+++ src/web_server/web_server.cpp
@@ -1,5 +1,6 @@
 #include "controllers/QueryController.cpp"
 #include <drogon/drogon.h>
+#include <filesystem>
 #include <iostream>
 
 using namespace std;
@@ -9,18 +10,23 @@ namespace ml_log::web_server {
 
 class WebServer {
   public:
+    // Attributes.
+    string web_client_root = "./web/build";
+
+    // Methods.
     void startWebServer();
-    WebServer(int port);
+
+    // Constructors.
+    explicit WebServer(int port) : _port(port) {}
 
   private:
     int _port;
 };
 
-WebServer::WebServer(int port) { this->_port = port; }
-
 void WebServer::startWebServer() {
     app().addListener("0.0.0.0", this->_port);
-    app().setDocumentRoot("./web/build");
+
+    app().setDocumentRoot(this->web_client_root);
 
     auto queryController = std::make_shared<QueryController>();
     app().registerController(queryController);
